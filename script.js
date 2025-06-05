c3786c521bbf8abd877660451d15626af7b18d64
ZalaNidhish <153850082+ZalaNidhish@users.noreply.github.com>
2025-06-05 22:40:50 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,50 +1,17 @@
 audiogo = new Audio('outsound.mp3');
 audio   = new Audio('bgmusic.mp3');
 audiojump= new Audio('jumpsound.mp3');
-
 score = 0;
+collision = false;
 
-document.onkeydown = function(e){
-	console.log("keycode is",e.keyCode);
-	if(e.keyCode==38){
-		jerry = document.querySelector('.jerry');
-		jerry.classList.add('aniJerry');
-		audiojump.play();
-		setTimeout(function(){
-			score+=10;
-			updateScore(score);
-			jerry.classList.remove('aniJerry')
-		},700);
-	}
-	
+player_scores = []
 
-	
-	if(e.keyCode==13){
-		tom = document.querySelector('.tom');
-		tom.classList.add('aniTom');
-		setTimeout(function(){
-		audio.play()
-		}, 100);
-		gameOver.style.visibility = 'hidden';
-	}
-	
-// if(e.keyCode==39){
-	
-// jerry = document.querySelector('.jerry');
-// jerryX=parseInt(window.getComputedStyle(jerry, null).getPropertyValue('left'));
-// jerry.style.left = jerryX + 100 +'px';
-	
-// }	
-// if(e.keyCode==37){
-	
-// jerry = document.querySelector('.jerry');
-// jerryX=parseInt(window.getComputedStyle(jerry, null).getPropertyValue('left'));
-// jerry.style.left = jerryX - 100 +'px';
-	
-}	
+function updateScore(score){
+	scoreCont.innerHTML = "YOUR SCORE:" + score;
+}
 
 
-setInterval(function(){
+function checkcollision(){
 	
 	jerry = document.querySelector('.jerry');
 	gameOver = document.querySelector('.gameOver');
@@ -59,13 +26,49 @@ setInterval(function(){
 	offsetY = Math.abs(jy-ty);
 	
 	if(offsetX<80 && offsetY<40){
+        collision = true;
+        player_scores.push(score)
+        console.log(player_scores);
 		gameOver.style.visibility = 'visible';
 		tom.classList.remove('aniTom');
 		audiogo.play();
 		audio.pause();
-	}		
-}, 100);
-
-function updateScore(score){
-	scoreCont.innerHTML = "YOUR SCORE:" +score;
+		audio.currentTime = 0;
+	}	
 }
+
+document.onkeydown = function(e){
+
+	if(e.keyCode==38 || e.keyCode==32){
+		audiojump.play();
+		jerry = document.querySelector('.jerry');
+		jerry.classList.add('aniJerry');
+		setTimeout(function(){
+			jerry.classList.remove('aniJerry');
+		},700);
+	}
+
+    if(e.keyCode==13 || e.keyCode==32){
+		tom = document.querySelector('.tom');
+		tom.classList.add('aniTom');
+
+           setInterval(() => {
+            if(collision==false){
+                score += 10
+                updateScore(score)
+            }
+           }, 500);
+                                
+                // setInterval(() => {                    
+                //     score += 10
+                //     updateScore(score)
+                // }, 1000);
+            }
+
+		setTimeout(function(){
+		audio.play()
+		},100);
+		gameOver.style.visibility = 'hidden';
+	}
+	
+setInterval(checkcollision, 100);
